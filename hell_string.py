699769acf1719898f6365529a115c54e6c7797a0
SMOKER_XD <83150771+smokerboy09@users.noreply.github.com>
2021-08-14 07:17:39 +0530

    Update hell_string.py

--- hell_string.py
+++ hell_string.py
@@ -14,7 +14,7 @@ if okvai == "69":
     with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
         print(client.session.save())
         client.send_message("me", client.session.save())
-        client.send_message("me", "Above is your #HELLBOT_SESSION \nPaste this string in Heroku Var.\n\n[Team HellBot](t.me/the_hellbot)")
+        client.send_message("me", "Above is your #SMØKERBØT_SESSION \nPaste this string in Heroku Var.\n\n[The SmøkerBøt](https://t.me/SMOKER_UB)"
 
 else:
-    print("Bhag jaa bhosdike")
+    print("JAA NAA RANDI")

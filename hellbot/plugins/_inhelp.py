4f92b6fb926c8855c52d546aa3f17c31ec3edc71
SMOKER_XD <83150771+smokerboy09@users.noreply.github.com>
2021-08-15 14:06:54 +0530

    Update _inhelp.py

--- hellbot/plugins/_inhelp.py
+++ hellbot/plugins/_inhelp.py
@@ -333,9 +333,9 @@ if Config.BOT_USERNAME is not None and tgbot is not None:
     async def on_plug_in_callback_query_handler(event):
         if event.query.user_id == bot.uid or event.query.user_id in Config.SUDO_USERS:
             veriler = custom.Button.inline(f"{hell_emoji} Re-Open Menu {hell_emoji}", data="reopen")
-            await event.edit(f"**⚜️ ☠ρøıƨøп ʙᴏᴛ☠ Mêñû Prõvîdêr ìs ñôw Çlösëd ⚜️**\n\n**Bot Of :**  {hell_mention}\n\n        [©️ Hêllẞø† ™️]({chnl_link})", buttons=veriler, link_preview=False)
+            await event.edit(f"**⚜️ ☠Smøkèr ʙᴏᴛ☠ Mêñû Prõvîdêr ìs ñôw Çlösëd ⚜️**\n\n**Bot Of :**  {hell_mention}\n\n        [©️ SmøkèrBøt ™️]({chnl_link})", buttons=veriler, link_preview=False)
         else:
-            reply_pop_up_alert = "Hoo gya aapka. Kabse tapar tapar dabae jaa rhe h. Khudka bna lo na agr chaiye to. © ☠ρøıƨøп ʙᴏᴛ☠ ™"
+            reply_pop_up_alert = "Hoo gya aapka. Kabse tapar tapar dabae jaa rhe h. Khudka bna lo na agr chaiye to. © ☠Smøkèr ʙᴏᴛ☠ ™"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
    
 

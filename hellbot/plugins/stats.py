f7b8966a1efe4088c0e0df457cbd1c3f3ff809cb
SMOKER_XD <83150771+smokerboy09@users.noreply.github.com>
2021-08-15 17:28:38 +0530

    Update stats.py

--- hellbot/plugins/stats.py
+++ hellbot/plugins/stats.py
@@ -62,23 +62,23 @@ async def stats(
         unread += dialog.unread_count
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
-    response = f"🔰**Stats for {full_name}**🔰\n\n"
-    response += f"🔱 **Private Chats:** {private_chats} \n"
-    response += f"🔸   `Users: {private_chats - bots}` \n"
-    response += f"🔹   `Bots: {bots}` \n"
-    response += f"🔱 **Groups:** {groups} \n"
-    response += f"🔱 **Channels:** {broadcast_channels} \n"
-    response += f"☣️  **Admin in Groups:** {admin_in_groups} \n"
-    response += f"🔹   `Creator: {creator_in_groups}` \n"
-    response += f"🔸   `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
-    response += f"☣️  **Admin in Channels:** {admin_in_broadcast_channels} \n"
-    response += f"🔸   `Creator: {creator_in_channels}` \n"
+    response = f"☠️**Stats for {full_name}**🔰\n\n"
+    response += f"☠️ **Private Chats:** {private_chats} \n"
+    response += f"☠️   `Users: {private_chats - bots}` \n"
+    response += f"☠️   `Bots: {bots}` \n"
+    response += f"☠️ **Groups:** {groups} \n"
+    response += f"☠️ **Channels:** {broadcast_channels} \n"
+    response += f"☠️  **Admin in Groups:** {admin_in_groups} \n"
+    response += f"☠️   `Creator: {creator_in_groups}` \n"
+    response += f"☠️   `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f"☠️  **Admin in Channels:** {admin_in_broadcast_channels} \n"
+    response += f"☠️   `Creator: {creator_in_channels}` \n"
     response += (
         f"🔹   `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
     )
-    response += f"🔱 **Unread:** {unread} \n"
-    response += f"🔱 **Unread Mentions:** {unread_mentions} \n\n"
-    response += f"☣️   __It Took:__ {stop_time:.02f}s \n"
+    response += f"☠️ **Unread:** {unread} \n"
+    response += f"☠️ **Unread Mentions:** {unread_mentions} \n\n"
+    response += f"☠️   __It Took:__ {stop_time:.02f}s \n"
     response += (
         f"📌 **From The DataBase Of** :- {hell_channel}"
     )

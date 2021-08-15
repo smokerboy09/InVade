ce0a50827fab34949c8acbedc50487526b917070
SMOKER_XD <83150771+smokerboy09@users.noreply.github.com>
2021-08-15 16:29:53 +0530

    Update federation.py

--- hellbot/plugins/federation.py
+++ hellbot/plugins/federation.py
@@ -101,7 +101,7 @@ async def _(event):
             await conv.get_response()
             await conv.send_message("/fedinfo " + lavde)
             massive = await conv.get_response()
-            await hell.edit(massive.text + "\n\n**ʟɛɢɛռɖaʀʏ_ᴀғ_ɦɛʟʟɮօt**")
+            await hell.edit(massive.text + "\n\n**ʟɛɢɛռɖaʀʏ_ᴀғ_SmøkèrBøt**")
         except YouBlockedUserError:
             await hell.edit("`Please Unblock` @MissRose_Bot")
             

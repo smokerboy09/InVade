3df3e52359a983f7cb3c4b0e3d1b79a2231c12a0
SMOKER_XD <83150771+smokerboy09@users.noreply.github.com>
2021-08-15 17:21:43 +0530

    Update rename.py

--- hellbot/plugins/rename.py
+++ hellbot/plugins/rename.py
@@ -172,7 +172,7 @@ async def _(event):
                     event.chat_id,
                     downloaded_file_name,
                     thumb=thumb,
-                    caption="reuploaded by [HellBot](https://t.me/hellbot_official_chat)",
+                    caption="reuploaded by [SMOKERBOT](https://t.me/smoker_ki_janta)",
                     force_document=False,
                     allow_cache=False,
                     reply_to=event.message.id,
